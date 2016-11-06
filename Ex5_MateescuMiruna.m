3d359c368328056a76bc8fb69ffc0eb4f1fd68fa
MirunaIoana <miruna.ioana5@gmail.com>
2016-11-06 16:37:00 +0200

    Update Ex5_MateescuMiruna.m

--- Ex5_MateescuMiruna.m
+++ Ex5_MateescuMiruna.m
@@ -1,4 +1,5 @@
 F = 50;
+
 t = 0:0.01:0.2;
 s=2*sin(2*pi*F*t);
 figure(1)
@@ -6,6 +7,7 @@ figure(1)
 plot(t,s,'.-')
 xlabel('Timp in [s]')
 grid
+
 t=0:0.001:0.2;
 s=2*sin(2*pi*F*t);
 figure(2)
@@ -13,6 +15,7 @@ figure(2)
 plot(t,s,'.-')
 xlabel('Timp in [s]')
 grid
+
 t=0:0.0002:0.2;
 s=2*sin(2*pi*F*t);
 figure(3)
@@ -20,12 +23,13 @@ figure(3)
 plot(t,s,'.-')
 xlabel('Timp in [s]')
 grid
-%perioada atunci cand t=0.01 este de T=20ms
-%perioada atunci cand t=0.0002 este de T=20ms
+%perioada atunci cand t=0.01 este de T=0.02s
+%perioada atunci cand t=0.0002 este de T=0.02s
+
 f=20;
-t=0:0.001:0.2;
-s=2*sin(2*pi*F*t);
-c=2*cos(2*pi*F*t);
+t=0:0.001:0.5;
+s=2*sin(2*pi*f*t);
+c=2*cos(2*pi*f*t);
 %generez un semnal cosinusoidal si il pun pe acelasi grafic peste semnalul
 %sinusoidal
 figure(4)
@@ -33,3 +37,4 @@ plot(t,s)
 grid
 hold on
 plot(t,c,'r')
+%curba pentru semnalul cosinusoidal va avea culoarea rosie
