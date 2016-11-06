a67b90ed3813f55e5fca0955ac6d9a757109cefc
MirunaIoana <miruna.ioana5@gmail.com>
2016-11-06 15:58:43 +0200

    Update TEMA_EX5_MateescuMiruna.m

--- TEMA_EX5_MateescuMiruna.m
+++ TEMA_EX5_MateescuMiruna.m
@@ -1,20 +1,26 @@
 T=4;
-f=2;
-%Semnal sinusoidal redresat dubl? alternan??
+f=0.25;
+%Semnal sinusoidal redresat dublu alternanta
 %Perioada semnalului neredresat este de 4s
 %Semnal cu rezolutie temporara 2ms
-t1=0:0.002:2;
-x1=abs(1.5*sin(2*pi*t1*2));
+t1=0:0.002:8;
+x1=abs(1.5*sin(2*pi*t1*f));
+%am pus functia in modul pentru a avea doar valori pozitive
+figure(1)
 plot(t1,x1)
 grid
-figure(2)
+
 %Semnal cu rezolutie temporara 20ms
-t2=0:0.02:2;
-x2=abs(1.5*sin(2*pi*t2*2));
+t2=0:0.02:8;
+x2=abs(1.5*sin(2*pi*t2*f));
+figure(2)
 plot(t2,x2)
-figure(3)
+
 %Semnal cu rezolutie temporara 200ms
-t3=0:0.2:2;
-x3=abs(1.5*sin(2*pi*t3*2));
-x3(x3<0)=0;
+t3=0:0.2:8;
+x3=abs(1.5*sin(2*pi*t3*f));
+figure(3)
 plot(t3,x3)
+
+%am afisat 4 perioade a unui semnal sinusoidal redresat dubla alternanta ce
+%neredresat avea 2 perioade
