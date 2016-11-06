dfe5f67034b6f75c9df4c75319bbabc5ab4dc076
MirunaIoana <miruna.ioana5@gmail.com>
2016-11-06 15:46:50 +0200

    Update TEMA_EX4_MateescuMiruna.m

--- TEMA_EX4_MateescuMiruna.m
+++ TEMA_EX4_MateescuMiruna.m
@@ -1,17 +1,30 @@
 T=3;
-f=3;
-t1=0:0.002:0.2
+f=0.33;
+%frecventa este de 0.33 (1/3)
+
+%generez un semnal cu rezolutie temporara de 2ms
+t1=0:0.002:9;
 x1=0.8*sin(2*pi*t1*f);
-x1(x1<0)=0
+x1(x1<0)=0;
+%daca x1 ia valori mai mici decat 0, x1 primeste 0
+figure(1)
 plot(t1,x1)
 grid
-figure(2)
-t2=0:0.02:2
+
+%generez un semnal cu rezolutie temporara de 20ms
+t2=0:0.02:9;
 x2=0.8*sin(2*pi*t2*f);
-x2(x2<0)=0
+x2(x2<0)=0;
+%daca x2 ia valori mai mici decat 0, x2 primeste 0
+figure(2)
 plot(t2,x2)
-figure(3)
-t3=0:0.2:20
+
+%generez un semnal cu rezolutie temporara de 200ms
+t3=0:0.2:9;
 x3=0.8*sin(2*pi*t3*f);
-x3(x3<0)=0
+x3(x3<0)=0;
+%daca x3 ia valori mai mici decat 0, x3 primeste 0
+figure(3)
 plot(t3,x3)
+
+%am afisat 3 semnale cu 3 perioade fiecare 
