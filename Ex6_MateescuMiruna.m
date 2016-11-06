59b0d53612851f89438929e7d4dd1f13bef77d86
MirunaIoana <miruna.ioana5@gmail.com>
2016-11-06 17:05:19 +0200

    Update Ex6_MateescuMiruna.m

--- Ex6_MateescuMiruna.m
+++ Ex6_MateescuMiruna.m
@@ -1,15 +1,20 @@
-Fs=12000
+Fs=12000;
+Fo=2000;
+%daca perioada este de 0.5ms atunci frecventa este de 2000Hz
 N=0.5*12;
+wo=2*pi*Fo/Fs;
 n=0:N-1;
-figure(1)
-t=0:0.1:0.5;
-s=abs(sin(pi*t*10/2))
+t=0:1/Fs:2;
+
+s=abs(sin(2*pi*t*2));
 %semnal binar aleator (valori 1 sau 0)
 %reprezint semnalul continuu in timp absolut folosind functia plot
+figure(1)
 plot(t,s)
 grid
+
+s=abs(sin(wo*n));
+%reprezint semnalul discret in functie de n folosind functia 
 figure(2)
-s=abs(sin(pi*n/2))
-%reprezint semnalul discret in functie de n folosind functia stem
 stem(n,s)
 grid
